5e87b278a15de33df803b30272c25a82ca05d30c
Nami <namnamsalimi@gmail.com>
2019-07-02 15:46:14 +0430

    Created Request and RequestBuilder classes

---
+++ sdk/src/main/java/co/omise/android/api/Request.kt
@@ -0,0 +1,18 @@
+package co.omise.android.api
+
+import co.omise.android.models.Model
+import okhttp3.HttpUrl
+import okhttp3.RequestBody
+
+/**
+ * Request is a base class, all extending classes would act as a holder class that encapsulate the information
+ * regarding which of the Omise APIs the user wants to access and pass any additional data needed for that api request
+ *
+ * @param <T> the generic type for any Model that would need to be returned by the {@link Client} when this request is passed to it
+ */
+open class Request<T : Model>(
+        internal val method: String,
+        internal val url: HttpUrl,
+        internal val payload: RequestBody?,
+        internal val responseType: Class<T>
+)
