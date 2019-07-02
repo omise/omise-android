774496d34ab683be3a0f656d2e88e3867938e0e6
Nami <namnamsalimi@gmail.com>
2019-07-02 15:48:28 +0430

    Refactored Client class to use Request in place of TokenRequest

--- sdk/src/main/java/co/omise/android/api/Client.kt
+++ sdk/src/main/java/co/omise/android/api/Client.kt
@@ -3,27 +3,20 @@ package co.omise.android.api
 import android.os.Build
 import android.os.Handler
 import co.omise.android.BuildConfig
-import co.omise.android.TokenRequest
-import co.omise.android.TokenRequestListener
+import co.omise.android.models.Model
 import okhttp3.*
-import java.security.GeneralSecurityException
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 
 /**
- * Client is the main entrypoint to the SDK. You can use the Client to send [TokenRequest]s.
- *
- * @see TokenRequest
- */
-class Client
-/**
- * Creates a Client with the given public Key.
+ * Client is the main entrypoint to the SDK and it needs to be supplied with a public Key. You can use the Client to send a [Request].
  *
  * @param publicKey The key with the `pkey_` prefix.
+ * @see TokenRequest
  */
-@Throws(GeneralSecurityException::class)
-constructor(private val publicKey: String) {
+class Client(private val publicKey: String) {
+
     private val httpClient: OkHttpClient
     private val background: Executor
 
@@ -38,12 +31,11 @@ constructor(private val publicKey: String) {
      * @param request  The request to send.
      * @param listener The listener to listen for request result.
      */
-    fun send(request: TokenRequest, listener: TokenRequestListener) {
+    fun <T : Model> send(request: Request<T>, listener: TokenRequestListener) {
         val handler = Handler()
         background.execute { Invocation(handler, httpClient, request, listener).invoke() }
     }
 
-    @Throws(GeneralSecurityException::class)
     private fun buildHttpClient(): OkHttpClient {
         val builder = OkHttpClient.Builder()
         val spec = ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
