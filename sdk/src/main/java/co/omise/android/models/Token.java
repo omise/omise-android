24f2f0b294302a955146d27cdb46aee5ce736a51
Nami <namnamsalimi@gmail.com>
2019-07-03 18:27:51 +0430

    Created RequestBuilder and used it in Token model to create a TokenRequest

--- sdk/src/main/java/co/omise/android/models/Token.java
+++ sdk/src/main/java/co/omise/android/models/Token.java
@@ -3,12 +3,97 @@ package co.omise.android.models;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import co.omise.android.SDKLog;
+import co.omise.android.api.RequestBuilder;
+import okhttp3.FormBody;
+import okhttp3.HttpUrl;
+import okhttp3.RequestBody;
 
 public class Token extends Model {
+    public final boolean used;
+    public final Card card;
+
+    public Token(String rawJson) throws JSONException {
+        this(new JSONObject(rawJson));
+    }
+
+    private Token(JSONObject json) throws JSONException {
+        super(json);
+        card = json.has("card") ? new Card(json.getJSONObject("card")) : null;
+        used = JSON.bool(json, "used");
+    }
+
+    public static class CreateTokenRequestBuilder extends RequestBuilder<Token> {
+        String name;
+        String number;
+        int expirationMonth;
+        int expirationYear;
+        String securityCode;
+        String city;
+        String postalCode;
+
+        public CreateTokenRequestBuilder(String name, String number, int expirationMonth, int expirationYear, String securityCode) {
+            this.name = name;
+            this.number = number;
+            this.expirationMonth = expirationMonth;
+            this.expirationYear = expirationYear;
+            this.securityCode = securityCode;
+        }
+
+        @NotNull
+        @Override
+        protected HttpUrl path() {
+            return HttpUrl.parse("https://vault.omise.co/tokens");
+        }
+
+        @Nullable
+        @Override
+        public RequestBody payload() {
+            FormBody.Builder builder = new FormBody.Builder()
+                    .add("card[name]", name)
+                    .add("card[number]", number)
+                    .add("card[expiration_month]", Integer.toString(expirationMonth))
+                    .add("card[expiration_year]", Integer.toString(expirationYear))
+                    .add("card[security_code]", securityCode);
+
+            if (city != null && !city.isEmpty()) {
+                builder.add("card[city]", city);
+            }
+            if (postalCode != null && !postalCode.isEmpty()) {
+                builder.add("card[postal_code]", postalCode);
+            }
+
+            return builder.build();
+        }
+
+        @NotNull
+        @Override
+        public String method() {
+            return RequestBuilder.POST;
+        }
+
+        @NotNull
+        @Override
+        protected Class<Token> type() {
+            return Token.class;
+        }
+
+        public CreateTokenRequestBuilder city(String city) {
+            this.city = city;
+            return this;
+        }
+
+        public CreateTokenRequestBuilder postalCode(String postalCode) {
+            this.postalCode = postalCode;
+            return this;
+        }
+    }
+
     public static final Parcelable.Creator<Token> CREATOR = new Creator<Token>() {
         @Override
         public Token createFromParcel(Parcel source) {
@@ -25,17 +110,4 @@ public class Token extends Model {
             return new Token[size];
         }
     };
-
-    public final boolean used;
-    public final Card card;
-
-    public Token(String rawJson) throws JSONException {
-        this(new JSONObject(rawJson));
-    }
-
-    public Token(JSONObject json) throws JSONException {
-        super(json);
-        card = json.has("card") ? new Card(json.getJSONObject("card")) : null;
-        used = JSON.bool(json, "used");
-    }
 }
