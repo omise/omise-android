5ec3a8ae0b26613f1a223c0955220e03cee1cb18
Nami <namnamsalimi@gmail.com>
2019-07-05 13:23:05 +0430

    Created Capability and PaymentMethod models

--- sdk/src/main/java/co/omise/android/models/JSON.java
+++ sdk/src/main/java/co/omise/android/models/JSON.java
@@ -3,8 +3,12 @@ package co.omise.android.models;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.ISODateTimeFormat;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 final class JSON {
     private static final DateTimeFormatter dateTimeFormatter = ISODateTimeFormat.dateTimeNoMillis();
@@ -29,5 +33,43 @@ final class JSON {
         return json.has(key) ?
                 DateTime.parse(json.getString(key), dateTimeFormatter) :
                 null;
+    }
+
+    public static List<String> stringList(JSONObject json, String key) throws JSONException {
+        List<String> list = new ArrayList<>();
+
+        JSONArray jArray = json.getJSONArray(key);
+        for (int i = 0; i < jArray.length(); i++) {
+            String str = jArray.getString(i);
+            list.add(str);
+        }
+        return list;
+    }
+
+    public static List<Integer> integerList(JSONObject json, String key) throws JSONException {
+        List<Integer> list = new ArrayList<>();
+
+        JSONArray jArray = json.getJSONArray(key);
+        for (int i = 0; i < jArray.length(); i++) {
+            int integer = jArray.getInt(i);
+            list.add(integer);
+        }
+
+        return list;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T extends Model> List<T> modelList(JSONObject json, String key, Class<T> clazz) throws JSONException {
+        List<T> list = new ArrayList<>();
+
+
+        JSONArray jArray = json.getJSONArray(key);
+        for (int i = 0; i < jArray.length(); i++) {
+            JSONObject object = jArray.getJSONObject(i);
+            Model model = ModelParserUtil.parseModelFromJson(object.toString(), clazz);
+            list.add((T) model);
+        }
+
+        return list;
     }
 }
