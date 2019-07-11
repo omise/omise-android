b82b2ae54b10779ee058f4cfd9b01a4ccc6f92c2
Nami <namnamsalimi@gmail.com>
2019-07-11 14:51:25 +0430

    Created custom ModelTypeResolver for Jackson to use for parsing Models

---
+++ sdk/src/main/java/co/omise/android/models/ModelTypeResolver.kt
@@ -0,0 +1,42 @@
+package co.omise.android.models
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+import com.fasterxml.jackson.databind.DatabindContext
+import com.fasterxml.jackson.databind.JavaType
+import com.fasterxml.jackson.databind.jsontype.impl.TypeIdResolverBase
+import java.util.*
+
+class ModelTypeResolver : TypeIdResolverBase() {
+    private var types: Map<String, Class<*>>? = null
+
+    fun getKnownTypes(): Map<String, Class<*>> {
+        if (types == null) {
+            types = HashMap()
+            (types as HashMap<String, Class<*>>)["card"] = Card::class.java
+            (types as HashMap<String, Class<*>>)["token"] = Token::class.java
+            (types as HashMap<String, Class<*>>)["capability"] = Capability::class.java
+            (types as HashMap<String, Class<*>>)["payment_method"] = PaymentMethod::class.java
+        }
+        return Collections.unmodifiableMap(types)
+    }
+
+    override fun idFromValue(value: Any): String {
+        return idFromValueAndType(value, value::class.java)
+    }
+
+    override fun idFromValueAndType(value: Any, suggestedType: Class<*>): String {
+        return reverse(getKnownTypes())[suggestedType] ?: ""
+    }
+
+    override fun getMechanism(): JsonTypeInfo.Id {
+        return JsonTypeInfo.Id.CUSTOM
+    }
+
+    private fun reverse(map: Map<String, Class<*>>): Map<Class<*>, String> {
+        val reversedMap = HashMap<Class<*>, String>()
+        for ((key, value) in map) {
+            reversedMap[value] = key
+        }
+        return reversedMap
+    }
+}
