63a3bb1f68d21bd42404bc5ddee72f7e2abfd661
Nami <namnamsalimi@gmail.com>
2019-07-11 19:41:55 +0430

    Created a Serializer to be used for parsing API responses into Models

---
+++ sdk/src/main/java/co/omise/android/models/Serializer.kt
@@ -0,0 +1,149 @@
+package co.omise.android.models
+
+import co.omise.android.api.RequestBuilder
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.joda.JodaModule
+import com.fasterxml.jackson.datatype.joda.cfg.JacksonJodaDateFormat
+import com.fasterxml.jackson.datatype.joda.ser.DateTimeSerializer
+import com.fasterxml.jackson.datatype.joda.ser.LocalDateSerializer
+import org.joda.time.DateTime
+import org.joda.time.LocalDate
+import org.joda.time.format.DateTimeFormatter
+import org.joda.time.format.ISODateTimeFormat
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+
+object Serializer {
+    /**
+     * Returns the pre-configured [ObjectMapper] used for
+     * serializing and deserializing Omise API objects.
+     *
+     * @return An [ObjectMapper] instance.
+     */
+    var objectMapper: ObjectMapper
+    /**
+     * Returns the pre-configured {@link DateTimeFormatter} used for
+     * serializing and deserializing date and times for Omise API objects.
+     *
+     * @return A {@link DateTimeFormatter} instance.
+     */
+    var dateTimeFormatter: DateTimeFormatter = ISODateTimeFormat.dateTimeNoMillis()
+    /**
+     * Returns the pre-configured {@link DateTimeFormatter} used for
+     * serializing and deserializing date for Omise API objects.
+     *
+     * @return A {@link DateTimeFormatter} instance.
+     */
+    var localDateFormatter: DateTimeFormatter = ISODateTimeFormat.date()
+
+    init {
+
+        objectMapper = ObjectMapper()
+                .registerModule(JodaModule()
+                        .addSerializer(DateTime::class.java, DateTimeSerializer()
+                                .withFormat(JacksonJodaDateFormat(dateTimeFormatter), 0)
+                        )
+                        .addSerializer(LocalDate::class.java, LocalDateSerializer()
+                                .withFormat(JacksonJodaDateFormat(localDateFormatter), 0)
+                        )
+                )
+
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE, true)
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                .configure(SerializationFeature.WRITE_NULL_MAP_VALUES, false) // TODO: Deprecate in vNext
+    }
+
+    /**
+     * Deserialize an instance of the given class from the input stream.
+     *
+     * @param input The [InputStream] that contains the data to deserialize.
+     * @param klass The [Class] to deserialize the result into.
+     * @param <T>   The type to deserialize the result into.
+     * @return An instance of type T deserialized from the input stream.
+     * @throws IOException on general I/O error.
+    </T> */
+    @Throws(IOException::class)
+    fun <T : Model> deserialize(input: InputStream, klass: Class<T>): T {
+        return objectMapper.readerFor(klass).readValue(input)
+    }
+
+    /**
+     * Deserialize an instance of the given type reference from the input stream.
+     *
+     * @param input The [InputStream] that contains the data to deserialize.
+     * @param ref   The [TypeReference] of the type to deserialize the result into.
+     * @param <T>   The type to deserialize the result into.
+     * @return An instance of the given type T deserialized from the input stream.
+     * @throws IOException on general I/O error.
+    </T> */
+    @Throws(IOException::class)
+    fun <T : Model> deserialize(input: InputStream, ref: TypeReference<T>): T {
+        return objectMapper.readerFor(ref).readValue(input)
+    }
+
+    /**
+     * Deserialize an instance of the given class from the map.
+     *
+     * @param map   The [Map] containing the JSON structure to deserialize from.
+     * @param klass The [Class] to deserialize the result into.
+     * @param <T>   The type to deserialize the result into.
+     * @return An instance of type T deserialized from the map.
+    </T> */
+    fun <T : Model> deserializeFromMap(map: Map<String, Any>, klass: Class<T>): T {
+        return objectMapper.convertValue(map, klass)
+    }
+
+    /**
+     * Deserialize an instance of the given type reference from the map.
+     *
+     * @param map The [Map] containing the JSON structure to deserialize from.
+     * @param ref The [TypeReference] of the type to deserialize the result into.
+     * @param <T> The type to deserialize the result into.
+     * @return An instance of the given type T deserialized from the map.
+    </T> */
+    fun <T : Model> deserializeFromMap(map: Map<String, Any>, ref: TypeReference<T>): T {
+        return objectMapper.convertValue(map, ref)
+    }
+
+    /**
+     * Serializes the given model to the output stream.
+     *
+     * @param output The [OutputStream] to serializes the model into.
+     * @param model  The [OmiseObject] to serialize.
+     * @param <T>    The type of the model to serialize.
+     * @throws IOException on general I/O error.
+    </T> */
+    @Throws(IOException::class)
+    fun <T : Model> serialize(output: OutputStream, model: T) {
+        objectMapper.writerFor(model::class.java).writeValue(output, model)
+    }
+
+    /**
+     * Serialize the given model to a map with JSON-like structure.
+     *
+     * @param model The [OmiseObject] to serialize.
+     * @param <T>   The type of the model to serialize.
+     * @return The map containing the model's data.
+    </T> */
+    fun <T : Model> serializeToMap(model: T): Map<String, Any> {
+        return objectMapper.convertValue(model, object : TypeReference<Map<String, Any>>() {})
+    }
+
+    /**
+     * Serializes the given [RequestBuilder] object to the provided output stream.
+     *
+     * @param outputStream The [OutputStream] to serialize the parameter into.
+     * @param builder      The [RequestBuilder] to serialize.
+     * @param <T>          The type of the parameter object to serialize.
+     * @throws IOException on general I/O error.
+    </T> */
+    @Throws(IOException::class)
+    fun <T : RequestBuilder<*>> serializeRequestBuilder(outputStream: OutputStream, builder: T) {
+        objectMapper.writerFor(builder.javaClass).writeValue(outputStream, builder)
+    }
+}
