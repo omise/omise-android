27efa5f4355c475ccbb1ca2885f5f7aa81f2399c
Natthawut Haematulin <natthawut1991@gmail.com>
2019-07-12 17:05:11 +0700

    Added simple error message to OmiseEditText.

--- sdk/src/main/java/co/omise/android/ui/OmiseEditText.kt
+++ sdk/src/main/java/co/omise/android/ui/OmiseEditText.kt
@@ -1,12 +1,54 @@
 package co.omise.android.ui
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.text.TextPaint
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatEditText
 
 
 open class OmiseEditText : AppCompatEditText {
-    constructor(context: Context?) : super(context)
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    private var errorText: TextPaint? = null
+
+    constructor(context: Context?) : super(context) {
+        init()
+    }
+
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+
+    var errorMessage: String? = null
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    private fun init() {
+
+        errorText = TextPaint().apply {
+            color = Color.RED
+            flags = Paint.ANTI_ALIAS_FLAG
+            textAlign = Paint.Align.LEFT
+        }
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+            canvas?.drawText(
+                    errorMessage ?: "",
+                    0f,
+                    y + height,
+                    errorText
+            )
+    }
 }
