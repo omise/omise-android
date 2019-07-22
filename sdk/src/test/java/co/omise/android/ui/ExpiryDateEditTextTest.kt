312d558ab6553589808428c79fbe6357c8ac1f90
Natthawut Haematulin <natthawut1991@gmail.com>
2019-07-22 16:50:44 +0700

    Added unit tests to test ExpiryDateEditText.

---
+++ sdk/src/test/java/co/omise/android/ui/ExpiryDateEditTextTest.kt
@@ -0,0 +1,64 @@
+package co.omise.android.ui
+
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ExpiryDateEditTextTest {
+
+    private val editText = ExpiryDateEditText(ApplicationProvider.getApplicationContext())
+
+    @Test
+    fun format_splitMonthAndYear() {
+        "1234".forEach { editText.append(it.toString()) }
+
+        assertEquals("12/34", editText.text.toString())
+        assertEquals(12, editText.expiryMonth)
+        assertEquals(2034, editText.expiryYear)
+    }
+
+    @Test
+    fun format_putZeroPrefix() {
+        "2".forEach { editText.append(it.toString()) }
+
+        assertEquals("02/", editText.text.toString())
+        assertEquals(2, editText.expiryMonth)
+        assertEquals(0, editText.expiryYear)
+    }
+
+    @Test
+    fun format_deleteYearValue() {
+        "1234".forEach { editText.append(it.toString()) } // 12/34
+
+        editText.text!!.delete(4, 5)
+        editText.text!!.delete(3, 4)
+
+        assertEquals("12/", editText.text.toString())
+        assertEquals(12, editText.expiryMonth)
+        assertEquals(0, editText.expiryYear)
+    }
+
+    @Test
+    fun validate_validateValue() {
+        "1234".forEach { editText.append(it.toString()) } // 12/34
+
+        assertEquals(emptyList<InvalidationType>(), editText.validate())
+    }
+
+    @Test
+    fun validate_emptyValue() {
+        "".forEach { editText.append(it.toString()) }
+
+        assertEquals(listOf(InvalidationType.Empty), editText.validate())
+    }
+
+    @Test
+    fun validate_invalidateWithIncompleteYearValue() {
+        "123".forEach { editText.append(it.toString()) } // 12/09
+
+        assertEquals(listOf(InvalidationType.Invalid), editText.validate())
+    }
+}
