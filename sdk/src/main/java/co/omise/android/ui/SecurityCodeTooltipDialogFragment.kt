be11cf0fadea416619befaba0bc76b3c2a088536
Natthawut Haematulin <natthawut1991@gmail.com>
2019-08-02 13:42:52 +0700

    Added CVV images. Set info for AMEX.

--- sdk/src/main/java/co/omise/android/ui/SecurityCodeTooltipDialogFragment.kt
+++ sdk/src/main/java/co/omise/android/ui/SecurityCodeTooltipDialogFragment.kt
@@ -1,15 +1,16 @@
 package co.omise.android.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.DialogFragment
 import co.omise.android.R
 import co.omise.android.models.CardBrand
+import kotlinx.android.synthetic.main.dialog_security_code_tooltip.close_button
 import kotlinx.android.synthetic.main.dialog_security_code_tooltip.cvv_description_text
 import kotlinx.android.synthetic.main.dialog_security_code_tooltip.cvv_image
 
@@ -18,6 +19,7 @@ class SecurityCodeTooltipDialogFragment : DialogFragment() {
 
     private val cvvImage: ImageView by lazy { cvv_image }
     private val cvvDescriptionText: TextView by lazy { cvv_description_text }
+    private val closeButton: ImageButton by lazy { close_button }
     private lateinit var cardBrand: CardBrand
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +36,18 @@ class SecurityCodeTooltipDialogFragment : DialogFragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        Log.d("cvv", cardBrand.name)
+        closeButton.setOnClickListener{ dismiss() }
+
+        when (cardBrand) {
+            CardBrand.AMEX -> {
+                cvvImage.setImageResource(R.drawable.cvv_4_digits)
+                cvvDescriptionText.setText(R.string.cvv_tooltip_4_digits)
+            }
+            else -> {
+                cvvImage.setImageResource(R.drawable.cvv_3_digits)
+                cvvDescriptionText.setText(R.string.cvv_tooltip_3_digits)
+            }
+        }
     }
 
     companion object {
