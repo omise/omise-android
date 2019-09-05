8edfc9b8c7cfe06bd5691e1dd68e0b26c003a1f6
Nami <namnamsalimi@gmail.com>
2019-09-05 16:57:40 +0700

    Fix base class serialization (#79)
    * Changed Model into an interface and also created a BaseModel to keep the Jackson annotations there
    
    * Changed all previous Model children to implement the Model interface and extend from BaseModel
    
    * Fixed some broken tests in ModelTest and ParcelableTest classes
    
    * Fixed test data to prevent SerializationTest test breaking
    
    * Deleted BaseModel class and moved the annotations from that class to the Model interface and also removed the redundant Parcelable implementation from all Model classes
    
    * Added kotlinOptions to sdk gradle configuration to try and fix breaking circle ci test
    
    * Revert "Added kotlinOptions to sdk gradle configuration to try and fix breaking circle ci test"
    
    This reverts commit 4989b911ed37df8678e2749a452c2ec6298f98c6.

--- sdk/src/main/java/co/omise/android/models/Model.kt
+++ sdk/src/main/java/co/omise/android/models/Model.kt
@@ -4,42 +4,17 @@ import android.os.Parcelable
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonTypeInfo
 import com.fasterxml.jackson.databind.annotation.JsonTypeIdResolver
-import kotlinx.android.parcel.Parcelize
 import org.joda.time.DateTime
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.CUSTOM, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "object", visible = true)
 @JsonTypeIdResolver(ModelTypeResolver::class)
-@Parcelize
-open class Model(
-        @field:JsonProperty("object")
-        val modelObject: String? = null,
-        val id: String? = null,
-        @field:JsonProperty("livemode")
-        val livemode: Boolean = false,
-        val location: String? = null,
-        @field:JsonProperty("created_at")
-        val created: DateTime? = null,
-        @field:JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-        val deleted: Boolean = false
-) : Parcelable {
-
-    override fun equals(other: Any?): Boolean {
-        val otherModel = other as Model
-        return otherModel.id == id &&
-                otherModel.modelObject == modelObject &&
-                otherModel.livemode == livemode &&
-                otherModel.location == location &&
-                otherModel.created?.millis == created?.millis &&
-                otherModel.deleted == deleted
-    }
-
-    override fun hashCode(): Int {
-        var result = modelObject?.hashCode() ?: 0
-        result = 31 * result + (id?.hashCode() ?: 0)
-        result = 31 * result + livemode.hashCode()
-        result = 31 * result + (location?.hashCode() ?: 0)
-        result = 31 * result + (created?.hashCode() ?: 0)
-        result = 31 * result + deleted.hashCode()
-        return result
-    }
+interface Model : Parcelable {
+    @get:JsonProperty("object")
+    var modelObject: String?
+    var id: String?
+    var livemode: Boolean
+    var location: String?
+    @get:JsonProperty("created_at")
+    var created: DateTime?
+    var deleted: Boolean
 }
