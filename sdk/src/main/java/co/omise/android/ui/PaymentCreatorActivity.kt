cc6a3522f1207cbaf27c535548bf72f65ac4204c
Natthawut Haematulin <natthawut1991@gmail.com>
2019-09-09 17:27:09 +0700

    Payment chooser page (#82)
    * Classify type of payment method.
    
    * Created PaymentCreatorActivity with required capability object.
    
    * Created base Fragment for displaying list itemes.
    
    * Displayed payment methods from capability object.
    
    * Set navigationn to PaymentChooserFragment.
    
    * Added close menu.
    
    * Added PaymentCreatorActivity test.
    
    * Added test for PaymentChooserFragment.
    
    * Updated sample app.
    
    * Deleted passing list to OmiseListFragment.
    
    * Deleted unnecessary Parcelable from PaymentMethod.

---
+++ sdk/src/main/java/co/omise/android/ui/PaymentCreatorActivity.kt
@@ -0,0 +1,83 @@
+package co.omise.android.ui
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import co.omise.android.R
+import co.omise.android.models.Capability
+
+class PaymentCreatorActivity : OmiseActivity() {
+
+    private val pkey: String by lazy { intent.getStringExtra(EXTRA_PKEY) }
+    private val amount: Long by lazy { intent.getLongExtra(EXTRA_AMOUNT, 0) }
+    private val currency: String by lazy { intent.getStringExtra(EXTRA_CURRENCY) }
+    private val capability: Capability by lazy { intent.getParcelableExtra<Capability>(EXTRA_CAPABILITY) }
+
+    private val navigation: PaymentCreatorNavigation by lazy { PaymentCreatorNavigationImpl(this) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment_creator)
+
+        listOf(EXTRA_PKEY, EXTRA_AMOUNT, EXTRA_CURRENCY, EXTRA_CAPABILITY).forEach {
+            require(intent.hasExtra(it)) { "Can not found $it." }
+        }
+
+        navigation.navigateToPaymentChooser(capability)
+    }
+
+    override fun onBackPressed() {
+        if (supportFragmentManager.findFragmentById(R.id.payment_creator_container) is PaymentChooserFragment) {
+            setResult(Activity.RESULT_CANCELED)
+            finish()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
+
+interface PaymentCreatorNavigation {
+    fun navigateToPaymentChooser(capability: Capability)
+    fun navigateToCreditCardForm()
+    fun navigateToInternetBankingChooser()
+    fun navigateToInstallmentChooser()
+    fun navigateToEContextForm()
+}
+
+open class PaymentCreatorNavigationImpl(activity: PaymentCreatorActivity) : PaymentCreatorNavigation {
+    companion object {
+        const val FRAGMENT_STACK = "PaymentCreatorNavigation.fragmentStack"
+    }
+
+    private val supportFragmentManager = activity.supportFragmentManager
+
+    private fun addFragmentToBackStack(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+                .add(R.id.payment_creator_container, fragment)
+                .addToBackStack(FRAGMENT_STACK)
+                .commit()
+    }
+
+    override fun navigateToPaymentChooser(capability: Capability) {
+        val fragment = PaymentChooserFragment.newInstance(capability).apply {
+            navigation = this@PaymentCreatorNavigationImpl
+        }
+        addFragmentToBackStack(fragment)
+    }
+
+    override fun navigateToCreditCardForm() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun navigateToInternetBankingChooser() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun navigateToInstallmentChooser() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun navigateToEContextForm() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
