f436783e1dfd3884bd10faeba0c5d6868e9019fb
Natthawut Haematulin <natthawut1991@gmail.com>
2019-09-12 10:38:26 +0700

    Disabled UI when make a request.

--- sdk/src/main/java/co/omise/android/ui/InternetBankingChooserFragment.kt
+++ sdk/src/main/java/co/omise/android/ui/InternetBankingChooserFragment.kt
@@ -11,25 +11,28 @@ import co.omise.android.models.backendType
 
 internal class InternetBankingChooserFragment : OmiseListFragment<InternetBankingChooserItem>() {
 
-    //    var paymentCreatorFlow: PaymentCreatorFlow? = null
-    private val availableBanks: List<SourceType.InternetBanking> by lazy {
+    private val allowedBanks: List<SourceType.InternetBanking> by lazy {
         val args = arguments ?: return@lazy emptyList<SourceType.InternetBanking>()
         val paymentMethods = args.getParcelableArray(EXTRA_INTERNET_BANKING_METHODS) as Array<PaymentMethod>
-        return@lazy paymentMethods.filter { it.backendType is BackendType.Source && (it.backendType as BackendType.Source).sourceType is SourceType.InternetBanking }
+        return@lazy paymentMethods
+                .filter { it.backendType is BackendType.Source && (it.backendType as BackendType.Source).sourceType is SourceType.InternetBanking }
                 .map { (it.backendType as BackendType.Source).sourceType as SourceType.InternetBanking }
     }
 
     var requester: PaymentCreatorRequester<Source>? = null
-    var listener: InternetBankingChooserFragmentListener? = null
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        title = "Internet Banking"
+        title = getString(R.string.internet_banking_chooser_title)
         setHasOptionsMenu(true)
     }
 
     override fun onListItemClicked(item: InternetBankingChooserItem) {
+        val req = requester ?: return
+
+        setUiEnabled(false)
+
         val sourceType = when (item) {
             InternetBankingChooserItem.Bbl -> SourceType.InternetBanking.Bbl
             InternetBankingChooserItem.Scb -> SourceType.InternetBanking.Scb
@@ -38,19 +41,14 @@ internal class InternetBankingChooserFragment : OmiseListFragment<InternetBankin
             is InternetBankingChooserItem.Unknown -> SourceType.Unknown(item.bankName)
         }
 
-        val req = requester ?: return
-
         val request = Source.CreateSourceRequestBuilder(req.amount, req.currency, sourceType).build()
         requester?.request(request) {
-            if (it.isSuccess) {
-
-            } else {
-            }
+            setUiEnabled(true)
         }
     }
 
     override fun listItems(): List<InternetBankingChooserItem> {
-        return availableBanks.map {
+        return allowedBanks.map {
             when (it) {
                 SourceType.InternetBanking.Bbl -> InternetBankingChooserItem.Bbl
                 SourceType.InternetBanking.Scb -> InternetBankingChooserItem.Scb
@@ -63,6 +61,7 @@ internal class InternetBankingChooserFragment : OmiseListFragment<InternetBankin
 
     companion object {
         private const val EXTRA_INTERNET_BANKING_METHODS = "InternetBankingChooserFragment.internetBankingMethods"
+
         fun newInstance(availableBanks: List<PaymentMethod>) =
                 InternetBankingChooserFragment().apply {
                     arguments = Bundle().apply {
@@ -77,22 +76,9 @@ sealed class InternetBankingChooserItem(
         override val title: String,
         override val indicatorIcon: Int
 ) : OmiseListItem {
-    companion object
     object Bbl : InternetBankingChooserItem(R.drawable.payment_bbl, "Bangkok Bank", R.drawable.ic_redirect)
     object Scb : InternetBankingChooserItem(R.drawable.payment_scb, "Siam Commercial Bank", R.drawable.ic_redirect)
     object Bay : InternetBankingChooserItem(R.drawable.payment_bay, "Bank of Ayudhya", R.drawable.ic_redirect)
     object Ktb : InternetBankingChooserItem(R.drawable.payment_ktb, "Krungthai Bank", R.drawable.ic_redirect)
     data class Unknown(val bankName: String) : InternetBankingChooserItem(R.drawable.payment_banking, bankName, R.drawable.ic_redirect)
-}
-
-val InternetBankingChooserItem.Companion.allElements: List<InternetBankingChooserItem>
-    get() = listOf(
-            InternetBankingChooserItem.Bbl,
-            InternetBankingChooserItem.Scb,
-            InternetBankingChooserItem.Bay,
-            InternetBankingChooserItem.Ktb
-    )
-
-interface InternetBankingChooserFragmentListener {
-    fun onCreateSourceCompleted(result: Result<Source>)
 }
