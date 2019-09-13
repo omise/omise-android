1ef79dab98cb3933aaf21cf388450b08a8a517a0
Natthawut Haematulin <natthawut1991@gmail.com>
2019-09-13 10:43:29 +0700

    Fixed displaying ActionBar's title.

--- sdk/src/main/java/co/omise/android/ui/OmiseFragment.kt
+++ sdk/src/main/java/co/omise/android/ui/OmiseFragment.kt
@@ -1,17 +1,28 @@
 package co.omise.android.ui
 
+import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import kotlin.properties.Delegates
 
 
 abstract class OmiseFragment : Fragment() {
-    var title: String by Delegates.observable("", { _, _, newValue ->
-        (activity as? AppCompatActivity)?.supportActionBar?.title = newValue
-    })
+
+    var title: String? = null
+
+    protected val actionBar: ActionBar?
+        get() = (activity as? AppCompatActivity)?.supportActionBar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        fragmentManager?.addOnBackStackChangedListener {
+            actionBar?.title = title
+        }
+    }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         menu.clear()
