5f106a6844982b3e2d64bc36738b080f04a0013a
Natthawut Haematulin <natthawut1991@gmail.com>
2019-09-16 15:57:46 +0700

    Create payment from installment (#87)
    * Created InstallmentChooserFragment.
    
    * Implemented navigation to navigate to InstallmentChooserFragment.
    
    * Created InstallmentTermChooserFragment.
    
    * Fixed displaying ActionBar's title.
    
    * Sent creating source request when chose a installment term.
    
    * Added InstallmentTermChooserFragmentTest.
    
    * Set title from Bank name.

---
+++ sdk/src/main/java/co/omise/android/ui/InstallmentTermChooserFragment.kt
@@ -0,0 +1,71 @@
+package co.omise.android.ui
+
+import android.os.Bundle
+import co.omise.android.R
+import co.omise.android.models.BackendType
+import co.omise.android.models.PaymentMethod
+import co.omise.android.models.Source
+import co.omise.android.models.SourceType
+import co.omise.android.models.backendType
+
+
+internal class InstallmentTermChooserFragment : OmiseListFragment<InstallmentTermChooserItem>() {
+    var requester: PaymentCreatorRequester<Source>? = null
+    private val installment: PaymentMethod? by lazy {
+        arguments?.getParcelable<PaymentMethod>(EXTRA_INSTALLMENT)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val sourceType = (installment?.backendType as BackendType.Source).sourceType
+        title = when (sourceType as SourceType.Installment) {
+            SourceType.Installment.Bay -> InstallmentChooserItem.Bay
+            SourceType.Installment.FirstChoice -> InstallmentChooserItem.FirstChoice
+            SourceType.Installment.Bbl -> InstallmentChooserItem.Bbl
+            SourceType.Installment.Ktc -> InstallmentChooserItem.Ktc
+            SourceType.Installment.KBank -> InstallmentChooserItem.KBank
+            is SourceType.Installment.Unknown -> InstallmentChooserItem.Unknown(sourceType.name.orEmpty())
+        }.title
+        setHasOptionsMenu(true)
+    }
+
+    override fun listItems(): List<InstallmentTermChooserItem> {
+        return installment?.installmentTerms.orEmpty().map {
+            InstallmentTermChooserItem(R.drawable.payment_installment, it, R.drawable.ic_redirect)
+        }
+    }
+
+    override fun onListItemClicked(item: InstallmentTermChooserItem) {
+        val req = requester ?: return
+        val sourceType = (installment?.backendType as? BackendType.Source)?.sourceType ?: return
+
+        setUiEnabled(false)
+        val request = Source.CreateSourceRequestBuilder(req.amount, req.currency, sourceType)
+                .installmentTerm(item.installmentTerm)
+                .build()
+        requester?.request(request) {
+            setUiEnabled(true)
+        }
+    }
+
+    companion object {
+        private const val EXTRA_INSTALLMENT = "InstallmentTermChooserFragment.installment"
+        fun newInstance(installment: PaymentMethod) =
+                InstallmentTermChooserFragment().apply {
+                    val args = Bundle().apply {
+                        putParcelable(EXTRA_INSTALLMENT, installment)
+                    }
+                    arguments = args
+                }
+    }
+}
+
+internal data class InstallmentTermChooserItem(
+        override val icon: Int,
+        val installmentTerm: Int,
+        override val indicatorIcon: Int
+) : OmiseListItem {
+    override val title: String
+        get() = "$installmentTerm months"
+}
