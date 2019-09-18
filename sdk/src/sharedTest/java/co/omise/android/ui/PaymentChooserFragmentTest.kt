d66e54840e5a35de6293842f07294ddfa2f51e8a
Natthawut Haematulin <natthawut1991@gmail.com>
2019-09-18 09:52:39 +0700

    Create payment from Alipay / Bill payment Tesco Lotus (#89)
    * Implemented Alipay request.
    
    * Added press event to the bill payment Tesco Lotus.
    
    * Added test cases.
    
    * Fixed setup testing.
    
    * Added robolectric's default config.

--- sdk/src/sharedTest/java/co/omise/android/ui/PaymentChooserFragmentTest.kt
+++ sdk/src/sharedTest/java/co/omise/android/ui/PaymentChooserFragmentTest.kt
@@ -8,23 +8,28 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intending
 import androidx.test.espresso.intent.matcher.ComponentNameMatchers.hasClassName
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
-import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isEnabled
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import co.omise.android.R
 import co.omise.android.models.Capability
 import co.omise.android.models.PaymentMethod
+import co.omise.android.models.Source
 import co.omise.android.utils.itemCount
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
+import org.hamcrest.CoreMatchers.not
+import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -34,12 +39,15 @@ class PaymentChooserFragmentTest {
     private lateinit var scenario: ActivityScenario<TestFragmentActivity>
     private lateinit var fragment: PaymentChooserFragment
     private val mockNavigation: PaymentCreatorNavigation = mock()
-
-    @get:Rule
-    val intentRule = IntentsTestRule<TestFragmentActivity>(TestFragmentActivity::class.java)
+    private val mockRequester: PaymentCreatorRequester<Source> = mock {
+        on { amount }.doReturn(500000L)
+        on { currency }.doReturn("thb")
+    }
 
     @Before
     fun setUp() {
+        Intents.init()
+
         val paymentMethods = listOf(
                 PaymentMethod(name = "card"),
                 PaymentMethod(name = "installment_bay"),
@@ -61,6 +69,7 @@ class PaymentChooserFragmentTest {
 
         fragment = PaymentChooserFragment.newInstance(capability).apply {
             navigation = mockNavigation
+            requester = mockRequester
         }
 
         intending(hasComponent(hasClassName(TestFragmentActivity::class.java.name)))
@@ -70,6 +79,11 @@ class PaymentChooserFragmentTest {
             it.startActivityForResult(Intent(it, TestFragmentActivity::class.java), 0)
             it.replaceFragment(fragment)
         }
+    }
+
+    @After
+    fun tearDown() {
+        Intents.release()
     }
 
     @Test
@@ -122,5 +136,23 @@ class PaymentChooserFragmentTest {
                 PaymentMethod(name = "installment_ktc")
         )
         verify(fragment.navigation)?.navigateToInstallmentChooser(expectedMethods)
+    }
+
+    @Test
+    fun clickBillPaymentTescoLotusPaymentMethod_sendRequestToCreateSource() {
+        onView(withId(R.id.recycler_view))
+                .perform(actionOnItemAtPosition<OmiseItemViewHolder>(3, click()))
+
+        onView(withId(R.id.recycler_view)).check(matches(not(isEnabled())))
+        verify(mockRequester).request(any(), any())
+    }
+
+    @Test
+    fun clickAlipayPaymentMethod_sendRequestToCreateSource() {
+        onView(withId(R.id.recycler_view))
+                .perform(actionOnItemAtPosition<OmiseItemViewHolder>(7, click()))
+
+        onView(withId(R.id.recycler_view)).check(matches(not(isEnabled())))
+        verify(mockRequester).request(any(), any())
     }
 }
