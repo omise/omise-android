59fe056bdb3962f0e2de6a2b1f778996f8e8ef37
Natthawut Haematulin <natthawut1991@gmail.com>
2019-10-24 11:21:46 +0700

    Put API request log.

--- sdk/src/main/java/co/omise/android/api/Configurer.kt
+++ sdk/src/main/java/co/omise/android/api/Configurer.kt
@@ -1,5 +1,7 @@
 package co.omise.android.api
 
+import android.util.Log
+import co.omise.android.BuildConfig
 import okhttp3.Credentials
 import okhttp3.Interceptor
 import okhttp3.Request
@@ -17,7 +19,18 @@ import okhttp3.Response
 class Configurer internal constructor(private val config: Config) : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
-        return chain.proceed(configure(config, chain.request()))
+        val request = chain.request()
+        if (BuildConfig.DEBUG) {
+            Log.i("APIRequest", "${request.method()} ${request.url().encodedPath()}")
+        }
+
+        val response = chain.proceed(configure(config, chain.request()))
+
+        if (BuildConfig.DEBUG) {
+            Log.i("APIRequest", "${request.method()} ${request.url().encodedPath()} - ${response.code()}")
+        }
+
+        return response
     }
 
     companion object {
