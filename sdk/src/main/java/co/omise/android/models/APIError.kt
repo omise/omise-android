e4edade0223f8d4a59a7b2dff29805ee94b515ee
Nami <namnamsalimi@gmail.com>
2019-10-24 11:16:31 +0700

    Added documentation for APIError class and also removed created field from the model

--- sdk/src/main/java/co/omise/android/models/APIError.kt
+++ sdk/src/main/java/co/omise/android/models/APIError.kt
@@ -1,15 +1,16 @@
 package co.omise.android.models
 
-import org.joda.time.DateTime
-
 /**
  * Represents Error object.
+ *
+ * @param location API documentation location.
+ * @param code Error code.
+ * @param message Error explanation.
  *
  * @see <a href="https://www.omise.co/errors-api">Errors API</a>
  */
 data class APIError(
         val location: String? = null,
         val code: String? = null,
-        override val message: String? = null,
-        val created: DateTime? = null
+        override val message: String? = null
 ) : Error()
