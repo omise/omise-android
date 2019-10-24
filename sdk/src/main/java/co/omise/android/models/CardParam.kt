5d406e6dc19d99d96fa2d82b00907682c5a6d82b
Nami <namnamsalimi@gmail.com>
2019-10-24 17:10:01 +0700

    Added documentation for CardParam class

--- sdk/src/main/java/co/omise/android/models/CardParam.kt
+++ sdk/src/main/java/co/omise/android/models/CardParam.kt
@@ -2,6 +2,12 @@ package co.omise.android.models
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
+/**
+ * CardParam holds all the information about a Card that can be supplied to create
+ * a [Token].
+ *
+ * @see [Token API](https://www.omise.co/tokens-api)
+ */
 data class CardParam(
         @field:JsonProperty
         val name: String,
