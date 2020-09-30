cad0d951168bed610560c2885a4db3ca71c1f04d
Natthawut Haematulin <natthawut1991@gmail.com>
2020-09-30 14:07:41 +0700

    feat: create ChargeStatus model

--- sdk/src/main/java/co/omise/android/models/Token.kt
+++ sdk/src/main/java/co/omise/android/models/Token.kt
@@ -4,6 +4,7 @@ import co.omise.android.api.Endpoint
 import co.omise.android.api.RequestBuilder
 import com.fasterxml.jackson.annotation.JsonProperty
 import kotlinx.android.parcel.Parcelize
+import kotlinx.android.parcel.TypeParceler
 import okhttp3.HttpUrl
 import okhttp3.RequestBody
 import org.joda.time.DateTime
@@ -15,11 +16,12 @@ import java.io.IOException
  * @see [Token API](https://www.omise.co/tokens-api)
  */
 @Parcelize
+@TypeParceler<ChargeStatus, ChargeStatusParceler>()
 data class Token(
         val used: Boolean = false,
         val card: Card? = null,
         @field:JsonProperty("charge_status")
-        val chargeStatus: String? = null,
+        val chargeStatus: ChargeStatus = ChargeStatus.Unknown,
         override var modelObject: String? = null,
         override var id: String? = null,
         override var livemode: Boolean = false,
