99621e7d977c9efd51c1587ff4f42df8b468a6e1
Natthawut Haematulin <natthawut1991@gmail.com>
2020-10-16 09:09:38 +0700

    Create a sample app for testing 3DS v2  (#152)
    * feat: add checkout button
    
    * feat: create PaymentProcessingActivity
    
    * feat: send creating charge request
    
    * feat: show payment result and complete checkout
    
    * fix: revise passing the result to the result page
    
    * feat: create new sample app for testing 3DS SDK
    
    * feat: delete unused code from 'app' module
    
    * feat: clean test data
    
    * feat: delete unused dependency

---
+++ sample-threeds-v2-app/src/main/java/co/omise/android/example/CheckoutActivity.kt
@@ -0,0 +1,87 @@
+package co.omise.android.example
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import co.omise.android.api.Client
+import co.omise.android.api.RequestListener
+import co.omise.android.models.Capability
+import co.omise.android.models.Token
+import co.omise.android.ui.CreditCardActivity
+import co.omise.android.ui.OmiseActivity
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_checkout.amount_edit
+import kotlinx.android.synthetic.main.activity_checkout.checkout_button
+
+class CheckoutActivity : AppCompatActivity() {
+
+    companion object {
+
+        const val PUBLIC_KEY = "[PUBLIC_KEY]"
+
+        private const val CHECKOUT_REQUEST_CODE = 0x3D8
+    }
+
+    private val amountEdit: EditText by lazy { amount_edit }
+    private val snackbar: Snackbar by lazy {
+        Snackbar.make(findViewById(R.id.content), "", Snackbar.LENGTH_SHORT)
+    }
+
+    private var capability: Capability? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checkout)
+
+        supportActionBar?.title = getString(R.string.activity_checkout)
+
+        checkout_button.setOnClickListener { checkout() }
+
+
+        val client = Client(PUBLIC_KEY)
+        val request = Capability.GetCapabilitiesRequestBuilder().build()
+        client.send(request, object : RequestListener<Capability> {
+            override fun onRequestSucceed(model: Capability) {
+                capability = model
+            }
+
+            override fun onRequestFailed(throwable: Throwable) {
+                snackbar.setText(throwable.message?.capitalize().orEmpty()).show()
+            }
+        })
+    }
+
+    private fun checkout() {
+        Intent(this, CreditCardActivity::class.java).run {
+            putExtra(OmiseActivity.EXTRA_PKEY, PUBLIC_KEY)
+            startActivityForResult(this, CHECKOUT_REQUEST_CODE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == RESULT_CANCELED) {
+            snackbar.setText(R.string.payment_cancelled).show()
+            return
+        }
+
+        if (data == null) {
+            snackbar.setText(R.string.payment_success_but_no_result).show()
+            return
+        }
+
+        when (requestCode) {
+            CHECKOUT_REQUEST_CODE -> {
+                Intent(this, PaymentProcessingActivity::class.java).run {
+                    val token = data.getParcelableExtra<Token>(OmiseActivity.EXTRA_TOKEN_OBJECT)
+                    putExtra(OmiseActivity.EXTRA_TOKEN, token.id)
+                    putExtra(OmiseActivity.EXTRA_AMOUNT, amountEdit.text.toString().toLong())
+                    startActivity(this)
+                }
+            }
+            else -> {
+                super.onActivityResult(requestCode, resultCode, data)
+            }
+        }
+    }
+}
--- sdk/src/main/java/co/omise/android/ui/AuthorizingPaymentActivity.kt
+++ sdk/src/main/java/co/omise/android/ui/AuthorizingPaymentActivity.kt
@@ -44,7 +44,8 @@ class AuthorizingPaymentActivity : AppCompatActivity(), ThreeDSListener {
         }
     }
 
-    private val tokenID = intent.getStringExtra(OmiseActivity.EXTRA_TOKEN)
+    private val tokenID: String by lazy { intent.getStringExtra(OmiseActivity.EXTRA_TOKEN) }
+
     private lateinit var viewModel: AuthorizingPaymentViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -177,7 +178,6 @@ class AuthorizingPaymentActivity : AppCompatActivity(), ThreeDSListener {
     private fun authorizeFailed(error: Throwable? = null) {
         progressDialog.dismiss()
         val errorIntent = Intent().apply {
-            // TODO: Send appropriate error
             putExtra(OmiseActivity.EXTRA_ERROR, error?.message)
         }
         setResult(Activity.RESULT_CANCELED, errorIntent)
