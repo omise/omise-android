f832553c726966cdfb8af53cb681be4cacaef46b
Natthawut Haematulin <natthawut1991@gmail.com>
2020-11-06 14:28:58 +0700

    3DS SDK config (#158)
    * feat: create config classes without implementation
    
    * feat: add implementation detail in the ThreeDSConfig
    
    * feat: move ThreeDSConfig to 3DS SDK
    
    * feat: initialize ThreeDSConfig
    
    * feat: revamp AuthoriginPaymentConfig class
    
    * feat: update initialize AuthorizingPaymentConfig
    
    * feat: call 3DS initializer in onCreate()
    
    * feat: set accept range to sdkMaxTimeout parameter
    
    * feat: delete UI customization config in the sample app

---
+++ sdk/src/main/java/co/omise/android/config/ThreeDSConfig.kt
@@ -0,0 +1,37 @@
+package co.omise.android.config
+
+import androidx.annotation.IntRange
+import co.omise.android.threeds.customization.UiCustomization
+
+
+class ThreeDSConfig private constructor(internal val threeDSConfig: co.omise.android.threeds.core.ThreeDSConfig) {
+
+    companion object {
+        private var instance: ThreeDSConfig? = null
+        internal val default: ThreeDSConfig = Builder().build()
+
+        @JvmStatic
+        fun get(): ThreeDSConfig = instance ?: default
+    }
+
+    class Builder {
+        companion object {
+            const val DEFAULT_TIMEOUT = 5
+        }
+
+        private var uiCustomization: UiCustomization? = null
+        private var timeout: Int = DEFAULT_TIMEOUT
+
+        fun uiCustomization(uiCustomization: UiCustomization): Builder = apply {
+            this.uiCustomization = uiCustomization
+        }
+
+        fun timeout(@IntRange(from = 5, to = 99) timeout: Int): Builder = apply {
+            this.timeout = timeout
+        }
+
+        fun build(): ThreeDSConfig {
+            return ThreeDSConfig(co.omise.android.threeds.core.ThreeDSConfig(uiCustomization, timeout))
+        }
+    }
+}
