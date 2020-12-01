59fe77e7e3baef520556e4808158ab9484f069c1
Natthawut Haematulin <natthawut1991@gmail.com>
2020-12-01 15:04:14 +0700

    feat: extract endpoints to OmiseEndpoints file

---
+++ sdk/src/staging/java/co/omise/android/api/OmiseEndpoints.kt
@@ -0,0 +1,5 @@
+package co.omise.android.api
+
+
+internal const val OMISE_API = TODO("Replace with the staging api endpoint.")
+internal const val OMISE_VAULT = TODO("Replace the staging vault endpoint.")
