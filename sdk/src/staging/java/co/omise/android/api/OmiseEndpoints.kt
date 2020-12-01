411292dd2ebc0a5ebaab8025d58c64a47da8a40f
Natthawut Haematulin <natthawut1991@gmail.com>
2020-12-01 16:07:24 +0700

    feat: update build flavors

--- sdk/src/staging/java/co/omise/android/api/OmiseEndpoints.kt
+++ sdk/src/staging/java/co/omise/android/api/OmiseEndpoints.kt
@@ -1,5 +1,5 @@
 package co.omise.android.api
 
 
-internal const val OMISE_API = TODO("Replace with the staging api endpoint.")
-internal const val OMISE_VAULT = TODO("Replace the staging vault endpoint.")
+internal const val OMISE_API = "[ENTER_STAGING_API]"
+internal const val OMISE_VAULT = "[ENTER_STAGING_VAULT]"
