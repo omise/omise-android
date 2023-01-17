399b7b51ce5b0735cb878832b4d1a7afa3e40a85
Narumon Nakkarit <narumon.n@opn.ooo>
2023-01-17 09:54:06 +0700

    Fix PaymentChooserFragmentTest

--- sdk/src/sharedTest/java/co/omise/android/ui/PaymentChooserFragmentTest.kt
+++ sdk/src/sharedTest/java/co/omise/android/ui/PaymentChooserFragmentTest.kt
@@ -60,8 +60,6 @@ class PaymentChooserFragmentTest {
             PaymentMethod(name = "installment_uob"),
             PaymentMethod(name = "internet_banking_bay"),
             PaymentMethod(name = "internet_banking_bbl"),
-            PaymentMethod(name = "internet_banking_ktb"),
-            PaymentMethod(name = "internet_banking_scb"),
             PaymentMethod(name = "bill_payment_tesco_lotus"),
             PaymentMethod(name = "econtext"),
             PaymentMethod(name = "alipay"),
@@ -193,9 +191,7 @@ class PaymentChooserFragmentTest {
 
         val expectedMethods = listOf(
             PaymentMethod(name = "internet_banking_bay"),
-            PaymentMethod(name = "internet_banking_bbl"),
-            PaymentMethod(name = "internet_banking_ktb"),
-            PaymentMethod(name = "internet_banking_scb")
+            PaymentMethod(name = "internet_banking_bbl")
         )
         verify(fragment.navigation)?.navigateToInternetBankingChooser(expectedMethods)
     }
