32a3d9b54539a5264910c7268af07db2f1c6634f
AnasNaouchi <anas.n@opn.ooo>
2023-12-06 13:23:46 +0700

    Remove api key log

--- sdk/src/main/java/co/omise/android/ThreeDS2ServiceWrapper.kt
+++ sdk/src/main/java/co/omise/android/ThreeDS2ServiceWrapper.kt
@@ -2,7 +2,6 @@ package co.omise.android
 
 import android.app.Activity
 import android.content.Context
-import android.util.Log
 import com.netcetera.threeds.sdk.api.ThreeDS2Service
 import com.netcetera.threeds.sdk.api.configparameters.builder.ConfigurationBuilder
 import com.netcetera.threeds.sdk.api.configparameters.builder.SchemeConfiguration
@@ -37,7 +36,6 @@ internal class ThreeDS2ServiceWrapper(
                 .encryptionPublicKey(BuildConfig.DS_PUBLIC_KEY)
                 .rootPublicKey(BuildConfig.DS_PUBLIC_KEY)
                 .build()
-            Log.d("New api key",BuildConfig.NETCETERA_API_KEY)
             val configParameters = ConfigurationBuilder()
                 .apiKey(BuildConfig.NETCETERA_API_KEY)
                 .configureScheme(schemeConfig)
