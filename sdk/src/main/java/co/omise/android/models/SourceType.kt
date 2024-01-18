64f7b21958340354c56ed8c0a0d1dd6ed7e743a7
AnasNaouchi <anas.n@opn.ooo>
2024-01-18 12:51:26 +0700

    Format

--- sdk/src/main/java/co/omise/android/models/SourceType.kt
+++ sdk/src/main/java/co/omise/android/models/SourceType.kt
@@ -71,6 +71,7 @@ sealed class SourceType(
     data class GrabPay(val provider: String? = null) : SourceType("grabpay")
 
     object PayPay : SourceType("paypay")
+
     object WeChatPay : SourceType("wechat_pay")
 
     data class Unknown(override val name: String?) : SourceType(name)
