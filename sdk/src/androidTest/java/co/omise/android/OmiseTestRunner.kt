faebe5e8af7b9e4b534656626b572e251dfb0d37
anas.n <anas.n@opn.ooo>
2026-04-24 03:54:35 +0700

    Fix Dexer and test syntax issues

--- sdk/src/androidTest/java/co/omise/android/OmiseTestRunner.kt
+++ sdk/src/androidTest/java/co/omise/android/OmiseTestRunner.kt
@@ -3,7 +3,6 @@ package co.omise.android
 import android.app.Application
 import android.content.Context
 import androidx.test.runner.AndroidJUnitRunner
-import com.github.tmurakami.dexopener.DexOpener
 
 class OmiseTestRunner : AndroidJUnitRunner() {
     override fun newApplication(
@@ -11,7 +10,6 @@ class OmiseTestRunner : AndroidJUnitRunner() {
         className: String?,
         context: Context?,
     ): Application {
-        DexOpener.install(this)
         return super.newApplication(cl, Application::class.java.name, context)
     }
 }
